8cd9c43a535509cd0e2343737770f40aa8dc7c0f
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-04 20:49:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,7 +61,6 @@ class Owner
   def sell_pets
     @pets.each{|type_of_pet, pet_array|
       pet_array.each{|pet|
-        binding.pry
         pet.mood ="nervous"
         pet_array.clear}}
   end
